d00ce84a32c84faea10db64615fe7b7aaedaa911
Jyoshna_2021 <jyoshna.s@tringapps.com>
2022-06-09 10:35:27 +0530

    Sorted list

--- app/src/main/java/com/example/sortedlist/MainActivity.kt
+++ app/src/main/java/com/example/sortedlist/MainActivity.kt
@@ -1,59 +1,95 @@
 package com.example.sortedlist
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
-import java.util.*
-import kotlin.collections.ArrayList
+import android.util.Log
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayoutStates.TAG
 
 class MainActivity : AppCompatActivity() {
-    var recyclerView: RecyclerView? = null
-    var mAdapter: RecyclerViewAdapter? = null
-    var Addbutton: Button? = null
-    var GetValue: EditText? = null
-    var ListElements = arrayOf("apple","android","ball","c","c++","cat","database","hen","ice",
-        "java","jack","json","lemon",)
-    var coordinatorLayout: CoordinatorLayout? = null
+
+    private var placesList = arrayListOf<Place>()
+    private lateinit var placesAdapter: PlacesAdapter
+    private lateinit var rvPlaces: RecyclerView
+    private lateinit var fabAddItem: Button
+    private lateinit var getValue: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recyclerView = findViewById(R.id.recyclerView)
-        coordinatorLayout = findViewById(R.id.coordinatorLayout)
 
-        val swipeToDeleteCallback: SwipeToDeleteCallback = object : SwipeToDeleteCallback(this) {
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, i: Int) {
-                val position = viewHolder.adapterPosition
-                mAdapter!!.removeItem(position)
-                val snackbar = Snackbar
-                    .make(
-                        coordinatorLayout!!,
-                        "Item was removed from the list.",
-                        Snackbar.LENGTH_LONG
-                    )
-               snackbar.show()
-            }
+        // initList()
+
+
+        initRecyclerView()
+
+        getValue=findViewById(R.id.edit_text)
+        fabAddItem = findViewById(R.id.fabAddItem)
+
+        fabAddItem.setOnClickListener {
+            val names = getValue.text.toString()
+            val list = ArrayList<Place>(placesList)
+            //creating new list from previous one
+            //adding new place object into it
+            list.add((Place(" $names")))
+
+            //passing it to our adapter or we can say AsyncListDiffer
+            list.sortBy { it.placeName.uppercase() }
+
+            placesAdapter.differ.submitList(list)
+            //list.sortBy { it.placeName.uppercase() }
+            //to scroll recyclerView to bottom
+
+            rvPlaces.smoothScrollToPosition(rvPlaces.adapter!!.itemCount)
+            placesList = list
+
         }
-        val itemTouchhelper = ItemTouchHelper(swipeToDeleteCallback)
-        itemTouchhelper.attachToRecyclerView(recyclerView)
 
-        val ListElementsArrayList: MutableList<String> = ArrayList(Arrays.asList(*ListElements))
-
-        Addbutton = findViewById<View>(R.id.button1) as Button
-        GetValue = findViewById<View>(R.id.editText1) as EditText
-        mAdapter = RecyclerViewAdapter(ListElementsArrayList as ArrayList<String>)
-        recyclerView!!.adapter = mAdapter
-        Addbutton!!.setOnClickListener {
-            ListElementsArrayList.add(GetValue!!.text.toString())
-            ListElementsArrayList.sort()
-            mAdapter!!.notifyDataSetChanged()
-        }
     }
+
+    private fun initRecyclerView() {
+        rvPlaces = findViewById(R.id.rvPlaces)
+        placesAdapter = PlacesAdapter()
+        rvPlaces.layoutManager = LinearLayoutManager(this)
+        rvPlaces.adapter = placesAdapter
+
+        ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(rvPlaces)
+
+        //pass list to adapter
+        placesAdapter.differ.submitList(placesList)
+    }
+    private val itemTouchHelperCallback =
+        object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ) = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                Log.d(TAG, "onSwiped: placesList ${placesList.size}")
+
+                val place = placesList[viewHolder.bindingAdapterPosition]
+
+                //creating new list from previous one
+                val list = ArrayList<Place>(placesList)
+                list.remove(place)
+                placesList = list
+                placesAdapter.differ.submitList(list)
+            }
+
+        }
+
+
+//    private fun initList() {
+//
+//        placesList.add(Place("Tomb"))
+//    }
+
 }
+
 
